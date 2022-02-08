264e8502edba0c9207cfa4e99df7aba7aff4b6f1
Robin Dittrich <robinodittrich@gmail.com>
2022-02-08 14:43:23 -0800

    Swap Faker strings to try and resolve postman errors.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Book.name }
-    description { Faker::Book.name }
+    name { Faker::Name.name }
+    description { Faker::Lorem.paragraph }
     unit_price { Faker::Number.number(digits: 4)}
     association :merchant, factory: :merchant
   end
