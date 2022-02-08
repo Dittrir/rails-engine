f5248b7ae2a1b8296a43293b83a73f022bbb59b5
Robin Dittrich <robinodittrich@gmail.com>
2022-02-07 22:09:14 -0800

    Remove useluss files.

--- app/controllers/api/v1/invoice_items_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::InvoiceItemsController < ApplicationController
-  def index
-    render(json: InvoiceItem.all)
-  end
-end
--- app/controllers/api/v1/invoices_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::InvoicesController < ApplicationController
-  def index
-    render(json: Invoice.all)
-  end
-end
