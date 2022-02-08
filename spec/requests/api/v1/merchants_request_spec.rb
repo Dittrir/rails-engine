9d2228e8a2e075528a9717532d5b1d785cb7ffab
Robin Dittrich <robinodittrich@gmail.com>
2022-02-08 14:50:00 -0800

    Add test to make sure empty arrays are returned if no results are found.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe 'The merchant API' do
     end
   end
 
+  it ' always return an array of data, even if one or zero resources are found' do
+    get '/api/v1/merchants'
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    merchants_data = merchants[:data]
+
+    expect(merchants_data).to be_an(Array)
+    expect(merchants_data.count).to eq(0)
+  end
+
   it 'can get one merchant by their id' do
     id = create(:merchant).id
 
