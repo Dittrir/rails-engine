581160abbc178d1438bbea58b1b59a3f299ce43f
Robin Dittrich <robinodittrich@gmail.com>
2022-02-07 22:27:26 -0800

    Align merchant request spec to align with serializer json format.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -45,21 +45,22 @@ RSpec.describe 'The merchant API' do
     get "/api/v1/merchants/#{id}/items"
 
     items = JSON.parse(response.body, symbolize_names: true)
+    items_data = items[:data]
 
     expect(response).to be_successful
 
-    items.each do |item|
-      expect(item).to have_key(:id)
-      expect(item[:id]).to eq(item[:id])
+    items_data.each do |item|
+      expect(item[:attributes]).to have_key(:id)
+      expect(item[:attributes][:id]).to eq(item[:attributes][:id])
 
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
 
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
 
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
 end
