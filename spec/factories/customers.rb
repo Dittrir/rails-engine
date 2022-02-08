b3526e057eb1b232a248336c6c016e1a777f1baf
Robin Dittrich <robinodittrich@gmail.com>
2022-02-07 22:11:29 -0800

    Remove files that are not in use.

--- spec/factories/customers.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :customer do
-    first_name { Faker::Book.name }
-    last_name { Faker::Book.name }
-  end
-end
--- spec/factories/invoice_items.rb
+++
@@ -1,8 +0,0 @@
-# FactoryBot.define do
-#   factory :invoice_item do
-#     item_id { Faker::Book.name }
-#     invoice_id { Faker::Book.name }
-#     quantity { Faker::Book.name }
-#     unit_price { Faker::Book.name }
-#   end
-# end
