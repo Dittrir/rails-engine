749d0b5bb773fadf0e5fda0e286531581bf8ba6b
Robin Dittrich <robinodittrich@gmail.com>
2022-02-08 10:42:11 -0800

    Add validations to merchant and item models.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  validates :name,
+            :presence => {message: "can't be blank"}
+  validates :description,
+            :presence => {message: "can't be blank"}
+  validates :unit_price,
+            :presence => {message: "can't be blank"},
+            :numericality => true
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  validates :name,
+            :presence => {message: "can't be blank"}
 end
