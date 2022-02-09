81c6e72c872c3b7dac376497c95c66f3080260c4
Robin Dittrich <robinodittrich@gmail.com>
2022-02-09 08:23:36 -0800

    Reorder routes.

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::SearchController < ApplicationController
+  def find_item
+    binding.pry
+  end
+
+  def find_all_items
+  end
+
   def find_merchant
   end
 
   def find_all_merchants
-  end
-
-  def find_item
-  end
-
-  def find_all_items
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      get '/merchants/:id/items', to: 'merchant_items#index'
+      get '/items/find', to: 'search#find_item'
+      get '/items/find_all', to: 'search#find_all_items'
+      
       get '/merchants/find', to: 'search#find_merchant'
       get '/merchants/find_all', to: 'search#find_all_merchants'
 
+      resources :merchants, only: [:index, :show]
+      get '/merchants/:id/items', to: 'merchant_items#index'
+
       resources :items, only: [:index, :show, :create, :update, :destroy]
       get '/items/:id/merchant', to: 'items_merchant#show'
-      get '/items/find', to: 'search#find_item'
-      get '/items/find_all', to: 'search#find_all_items'
     end
   end
 end
