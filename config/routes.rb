4026f5d7030238b0f046dd5c6c9a589fb5d994dc
Robin Dittrich <robinodittrich@gmail.com>
2022-02-09 08:05:36 -0800

    Create all search routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
-
       get '/merchants/:id/items', to: 'merchant_items#index'
+      get '/merchants/find', to: 'search#find_merchant'
+      get '/merchants/find_all', to: 'search#find_all_merchants'
 
       resources :items, only: [:index, :show, :create, :update, :destroy]
-
       get '/items/:id/merchant', to: 'items_merchant#show'
+      get '/items/find', to: 'search#find_item'
+      get '/items/find_all', to: 'search#find_all_items'
     end
   end
 end
