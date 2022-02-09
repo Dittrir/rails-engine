5849b0e219dd0f683e9ae12e5d04e0cb9348788c
Robin Dittrich <robinodittrich@gmail.com>
2022-02-09 09:56:50 -0800

    Create search controller action for find merchant

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,12 +1,30 @@
 class Api::V1::SearchController < ApplicationController
   def find_item
-    binding.pry
+    if params[:name].present?
+      item = Item.where("lower(name) LIKE ?", "%#{params[:name].downcase}%")
+                 .order(:name)
+                 .first
+      render(json: ItemSerializer.new(item), status: 200)
+    elsif params[:min_price].present?
+    elsif params[:max_price].present?
+    elsif params[:min_price].present? && params[:max_price].present?
+    else
+      render(json: {data: {message: "No Matches"}, status: 200})
+    end
   end
 
   def find_all_items
   end
 
   def find_merchant
+    if params[:name].present?
+      merchant = Merchant.where("lower(name) LIKE ?", "%#{params[:name].downcase}%")
+                 .order(:name)
+                 .first
+      render(json: MerchantSerializer.new(merchant), status: 200)
+    else
+      render(json: {data: {message: "Name cannot be missing"}, status: 200})
+    end
   end
 
   def find_all_merchants
